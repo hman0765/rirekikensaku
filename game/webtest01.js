7b0681704aa0e190d9e6d549005cf66a02f3c047
hman0765 <sheldot6027-tw8541@yahoo.co.jp>
2026-03-09 17:20:24 +0900

    Add files via upload

--- game/webtest01.js
+++ game/webtest01.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\yuki05\AppData\Local\Temp\tmpyp4g73_3.js
+// include: C:\Users\yuki05\AppData\Local\Temp\tmp6fedb5_f.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -206,21 +206,21 @@ Module['FS_createPath']("/assets", "stages", true, true);
 
   })();
 
-// end include: C:\Users\yuki05\AppData\Local\Temp\tmpyp4g73_3.js
-// include: C:\Users\yuki05\AppData\Local\Temp\tmp_qj38tix.js
+// end include: C:\Users\yuki05\AppData\Local\Temp\tmp6fedb5_f.js
+// include: C:\Users\yuki05\AppData\Local\Temp\tmp28nbl3aw.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\yuki05\AppData\Local\Temp\tmp_qj38tix.js
-// include: C:\Users\yuki05\AppData\Local\Temp\tmp80zdvbxx.js
+  // end include: C:\Users\yuki05\AppData\Local\Temp\tmp28nbl3aw.js
+// include: C:\Users\yuki05\AppData\Local\Temp\tmpbl5snqvq.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\yuki05\AppData\Local\Temp\tmp80zdvbxx.js
+  // end include: C:\Users\yuki05\AppData\Local\Temp\tmpbl5snqvq.js
 
 
 var arguments_ = [];
